59953df22799ec4678ff2c42cefb065e1301c464
David Dudas <david.dudas@outlook.com>
2026-06-20 11:38:43 +0200

    example code for MSP430G2210 to blink an LED on the MSP430 launchpad

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // Watchdog stop
 
-    P1DIR |= BIT0;             // P1.0 output
+    P1DIR |= BIT2;             // P1.2 output
 
     while(1)
     {
-        P1OUT ^= BIT0;         // Toggle LED
+        P1OUT ^= BIT2;         // Toggle LED
 
         volatile unsigned long i;
         for(i = 0; i < 50000; i++);
