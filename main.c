d538b6538e9874eff9c6004f0432ac7eeb9460a1
David Dudas <david.dudas@outlook.com>
2026-06-20 14:12:25 +0200

    initialize every pin as low output to decrease power consumption, turn on for 5s keep off for 10s

--- main.c
+++ main.c
@@ -2,52 +2,67 @@
 
 #define LED_PIN BIT2
 
-static void toggle_led(void)
+#define TICKS_5S   7500    // VLO ~12kHz / 8 = ~1500 Hz, 5s  = 7500
+#define TICKS_10S  15000   // 10s = 15000
+
+static void led_on(void)
 {
-    if (P1DIR & LED_PIN)
-    {
-        P1DIR &= ~LED_PIN;      // OFF = Hi-Z
-    }
-    else
-    {
-        P1OUT &= ~LED_PIN;
-        P1DIR |= LED_PIN;       // ON = sink
-    }
+    P1OUT &= ~LED_PIN;
+    P1DIR |= LED_PIN;       // ON = sink
+}
+
+static void led_off(void)
+{
+    P1DIR &= ~LED_PIN;      // OFF = Hi-Z
 }
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;
 
-    P1SEL &= ~LED_PIN;
+    // clear alternate function selection for all pins
+    P1SEL = 0x00;
 #ifdef P1SEL2
-    P1SEL2 &= ~LED_PIN;
+    P1SEL2 = 0x00;
 #endif
 
-    P1OUT &= ~LED_PIN;
-    P1DIR |= LED_PIN;           // start ON
+    // clear all outputs and set all pins to output low to decrease power consumption
+    P1OUT = 0x00;
+    P1DIR = 0xFF;      // all P1 pins output low
 
-    TACCR0 = 1500;
+#ifdef P2DIR
+    P2OUT = 0x00;
+    P2DIR = 0xFF;
+#endif
+
+    led_on();               // start ON
+
+    BCSCTL3 |= LFXT1S_2;    // ACLK = VLO ~12 kHz
+
+    TACCR0 = TICKS_5S;      // first interrupt after 5s
     TACCTL0 = CCIE;
-
-    BCSCTL3 |= LFXT1S_2;      // ACLK = VLO (~12 kHz)
-    TACTL = TASSEL_1 | ID_3 | MC_1 | TACLR;   // ACLK/8
+    TACTL = TASSEL_1 | ID_3 | MC_1 | TACLR;   // ACLK / 8, up mode
 
     __enable_interrupt();
 
     while (1)
     {
-        __bis_SR_register(LPM3_bits | GIE);  // sleep until Timer_A interrupt
+        __bis_SR_register(LPM3_bits | GIE);
     }
 }
 
 void __attribute__((interrupt(TIMERA0_VECTOR))) Timer_A_ISR(void)
 {
-    static unsigned int div = 0;
-
-    if (++div >= 3)
+    if (P1DIR & LED_PIN)
     {
-        div = 0;
-        toggle_led();
+        led_off();
+        TACCR0 = TICKS_10S;     // stay OFF for 10s
     }
+    else
+    {
+        led_on();
+        TACCR0 = TICKS_5S;      // stay ON for 5s
+    }
+
+    TACTL |= TACLR;             // restart timer period cleanly
 }
