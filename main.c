ed10a65703ab9da2f01903703338d0150bff161d
David Dudas <david.dudas@outlook.com>
2026-06-20 13:31:29 +0200

    start adding a timer

--- main.c
+++ main.c
@@ -1,23 +1,45 @@
 #include <msp430.h>
 
+#define LED_PIN BIT2
+
+static void toggle_led(void)
+{
+    if (P1DIR & LED_PIN)
+        P1DIR &= ~LED_PIN;      // OFF = Hi-Z
+    else {
+        P1OUT &= ~LED_PIN;
+        P1DIR |= LED_PIN;       // ON = sink
+    }
+}
+
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // Watchdog stop
+    WDTCTL = WDTPW | WDTHOLD;
 
-    volatile unsigned long i;
+    P1SEL &= ~LED_PIN;
+#ifdef P1SEL2
+    P1SEL2 &= ~LED_PIN;
+#endif
 
-    while(1)
+    P1OUT &= ~LED_PIN;
+    P1DIR |= LED_PIN;       // ON = sink
+
+    TACCR0 = 62500;
+    TACTL = TASSEL_2 | ID_3 | MC_1 | TACLR;   // SMCLK / 8, up mode
+
+    unsigned int div = 0;
+
+    while (1)
     {
-        // LED ON: drive low
-        P1DIR |= BIT2;     // P1.2 output
-        P1OUT &= ~BIT2;    // drive low
+        if (TACCTL0 & CCIFG)
+        {
+            TACCTL0 &= ~CCIFG;
 
-        for(i = 0; i < 50000; i++);
-
-        // LED OFF: high impedance
-        P1DIR &= ~BIT2;    // input = Hi-Z
-
-        for(i = 0; i < 50000; i++);
-
+            if (++div >= 3)
+            {
+                div = 0;
+                toggle_led();
+            }
+        }
     }
 }
